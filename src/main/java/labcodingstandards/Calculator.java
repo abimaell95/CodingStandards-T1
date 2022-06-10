58aea402df4ad466e455079168404a92e3f892ac
abimaell95 <gcemas20@gmail.com>
2022-06-10 15:22:02 -0500

    Fix Whitespace errors and upload checkstyle report
    Delete LabCodingStandards – Checkstyle Results _FixedErrors.pdf

--- src/main/java/labcodingstandards/Calculator.java
+++ src/main/java/labcodingstandards/Calculator.java
@@ -54,7 +54,7 @@ public class Calculator {
         }
 
         Calculator cal=new Calculator();
-        String result=cal.operation(first,second,operator);
+        String result=cal.operation(first, second, operator);
 
         System.out.printf(result);
 		reader.close();
